a0ea4908a793ee2626c0d7ae2f1f6478bf0714fb
Jonathan Delorme <delorme.jonathan@gmail.com>
2014-07-07 22:31:02 +0200

    Added Customer model & migration.

---
+++ server/app/database/migrations/2014_07_07_2017555_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('customers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->string('lastname');
+			$table->string('firstname');
+			$table->string('phone');
+			$table->string('address');
+			$table->integer('zip');
+			$table->string('city');
+			$table->string('country');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('customers');
+	}
+
+}
