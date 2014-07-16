d661f2dc0eb93eadfc7e1904647f2976a6fc233a
Jonathan Delorme <delorme.jonathan@gmail.com>
2014-07-16 21:50:16 +0200

    Added delivery code to confirmation email for drive.

--- server/app/views/emails/drive/order.blade.php
+++ server/app/views/emails/drive/order.blade.php
@@ -1,5 +1,7 @@
 <p>Bonjour,</p>
 
-<p>Merci pour votre commande passée sur radian-drive.fr</p>
+<p>Merci pour votre commande passée sur radian-drive.fr. Elle est enregistrée sous la référence {{ order.reference }}</p>
+
+<p><strong>Votre code de retrait est : {{ order.code }}</strong></p>
 
 <p>A bientôt.</p>
